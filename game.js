89409e12565d18ea4863a95163cf0ccc69c13a3a
Jack <cameron.jack.m@gmail.com>
2021-06-22 19:27:51 +1000

    simplify

--- game.js
+++ game.js
@@ -1,57 +1,16 @@
 function computerPlay(){
-let cpuNumber = Math.floor(Math.random() * 3);
-console.log(cpuNumber);
-
-switch (cpuNumber) {
-  case 0:
-   cpuSelection = "scissors";
-   console.log(cpuSelection);
-   break;
-   case 1:
-     cpuSelection = "paper";
-     console.log(cpuSelection);
-     break;
-     case 2:
-       cpuSelection = "rock";
-       console.log(cpuSelection);
-       break;           
-}
+let array = ["scissors", "paper", "rock"];
+computerSelection = 
+array[Math.floor(Math.random()*3)];
+console.log(computerSelection);
 } 
-// cpuNumber 0 = 'scissors, 1 = 'paper', 2 = 'rock'
-
-function playRound(cpuSelection, playerSelection) {
-switch (cpuSelection) {
-case cpuSelection == playerSelection:
-text = "Tie";
-break;
-case (cpuSelection = "scissors") && (playerSelection = "paper"):
-text = "scissors cut paper";
-break;
-case (cpuSelection = "scissors") && (playerSelection = "rock"):
-text = "rock crushes scissors";
-break;
-case (cpuSelection = "paper") && (playerSelection = "scissors"):
-
-
-
-
-
-  
-
- }
-
- }
-      
-
-
-
-  
-
-
-
-
-
- const playerSelection = "";
- const cpuSelection = computerPlay();
- console.log(playRound(cpuSelection, playerSelection));
-
+function playRound(playerSelection, computerSelection){
+switch (playerSelection){
+  case ((playerSelection == 'scissors' && computerSelection == 'paper') ||
+  (playerSelection == 'paper' && computerSelection == 'rock') ||
+  (playerSelection == 'rock' && computerSelection == 'scissors')):
+}
+}
+const playerSelection = "rock";
+const cpuSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));             
