f5ced8942db4c1df9d351874ef53b3837dea91a0
Jack <cameron.jack.m@gmail.com>
2021-06-24 17:08:07 +1000

    changes. but still no work

--- game.js
+++ game.js
@@ -2,15 +2,33 @@ function computerPlay(){
 let array = ["scissors", "paper", "rock"];
 computerSelection = 
 array[Math.floor(Math.random()*3)];
-console.log(computerSelection);
+return computerSelection;
 } 
 function playRound(playerSelection, computerSelection){
+  var result;
 switch (playerSelection){
-  case ((playerSelection == 'scissors' && computerSelection == 'paper') ||
+  case 'scissors' || 'paper' || 'rock':
+   if ((playerSelection == 'scissors' && computerSelection == 'paper') ||
   (playerSelection == 'paper' && computerSelection == 'rock') ||
-  (playerSelection == 'rock' && computerSelection == 'scissors')):
+  (playerSelection == 'rock' && computerSelection == 'scissors'));
+  result = 'You Win! you played {playerSelection} and computer played {computerSelection}';
+  break;
+
+  case 'scissors' || 'paper' || 'rock':
+   if ((playerSelection == 'scissors' && computerSelection == 'scissors') ||
+  (playerSelection == 'paper' && computerSelection == 'paper') ||
+  (playerSelection == 'rock' && computerSelection == 'rock'));
+  result = 'Tie! You played {playerSelection} and computer played {computerSelection}';
+  break;
+
+  default:
+  result = 'You Lose! You played {playerSelection} and computer played {computerSelection}'; 
+  break;
+
 }
+          
+
 }
 const playerSelection = "rock";
 const cpuSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));             
+// console.log(playRound(playerSelection, computerSelection)); 
